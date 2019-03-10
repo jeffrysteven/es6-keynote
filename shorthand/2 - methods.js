f08221bdeaaa5a4c95031e813734d6b1f005bcad
Jeffry Lenis <Jeffry@MacBook-Pro-de-Jeffry.local>
2019-03-10 17:48:20 -0500

    ES6: * Shorthand properties and methods.

---
+++ shorthand/2 - methods.js
@@ -0,0 +1,14 @@
+/* if you want to define an object who's keys have the same name as the variables passed-in as properties, 
+you can use the shorthand and simply pass the key name. */
+
+function formatMessage({ name, id, avatar }) {
+  return {
+    id,
+    name,
+    avatar,
+    timestamp: Date.now(),
+    delete () {
+      // delete message
+    }
+  }
+}
