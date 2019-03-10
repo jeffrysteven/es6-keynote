891030f53a7fe5e54f6dc82fe717c79688908978
Jeffry Lenis <Jeffry@MacBook-Pro-de-Jeffry.local>
2019-03-10 17:51:19 -0500

    * Shorthand properties and methods.

--- shorthand/2 - methods.js
+++ shorthand/2 - methods.js
@@ -1,7 +1,7 @@
 /* if you want to define an object who's keys have the same name as the variables passed-in as properties, 
 you can use the shorthand and simply pass the key name. */
 
-function formatMessage({ name, id, avatar }) {
+function formatMessage(name, id, avatar) {
   return {
     id,
     name,
